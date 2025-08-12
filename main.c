a27c07cf9ebc70c0ab2aebd7eb8c097e96962033
jculleto <jculleto@student.42singapore.sg>
2025-08-12 22:11:04 +0800

    push main

--- main.c
+++ main.c
@@ -3,10 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
+/*   By: jculleto <jculleto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 16:12:48 by amtan             #+#    #+#             */
-/*   Updated: 2025/08/12 16:12:49 by amtan            ###   ########.fr       */
+/*   Updated: 2025/08/12 21:46:13 by jculleto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "bsq.h"
+
+
+void	process_stdin(void)
+{
+	t_map	map;
+
+	if (parse_map(0, &map) == 0)
+	{
+		solve_map(&map);
+		print_map(&map);
+		free_map(&map);
+	}
+	else
+	{
+		write(1, "map error\n", 10);
+	}
+	return (0);
+
+}
+
+void	process_file(char *filename)
+{
+	t_map	map;
+	int		fd;
+
+
+	fd = open(argv[i], O_RDONLY);
+	if (fd < 0)
+		write(1, "map error\n", 10);
+	else
+		result = parse_map(fd, &map);
+	close(fd);
+	if (result == 0)
+	{
+		solve_map(&map);
+		print_map(&map);
+		free_map(&map);
+	}
+	else
+		write(1, "map error\n", 10);
+	close(fd);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+
+	if (argc < 2)
+	{
+		process_stdin()
+		retrun(0)
+	}
+		while (i < argc)
+	{
+		process_file(argv[i]);
+		if (i < argc - 1)
+			write(1, "\n", 1);
+		i++;
+	}
+}
